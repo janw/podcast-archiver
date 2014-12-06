88a39b005c1c1420fd88038e56c2f3470d8b53e5
Jan Willhaus <mail@janwillhaus.de>
2014-12-06 13:21:51 +0100

    Major refactoring, is now function with command line arg-ins

--- podcast_archiver.py
+++ podcast_archiver.py
@@ -1,43 +1,86 @@
 #!/usr/bin/env python
 
+import sys
+import getopt
 import feedparser
-from urllib import request
-import shutil
+from urllib.request import urlopen
+from shutil import copyfileobj
 from os import path
 
-savedir = path.expanduser('~/Music/Podcasts/Archive')
 
-nextPage = 'http://freakshow.fm/feed/m4a/'
-verbose = True
+verbose = 1
 
-if verbose:
-    print("1. Gathering link list ..", end="")
+def main():
+    global verbose
+    # Parse input arguments
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],
+                                   "f:d:v",
+                                   ["feed=", "dir=", "verbose"])
+    except getopt.GetoptError as error:
+        print("An error occured during input parsing: " + error.msg)
+        return
 
-linklist = []
-while nextPage is not None:
-    print(".", end="", flush=True)
-    feedobj = feedparser.parse(nextPage)
+    print(opts,args)
+    feedlist = []
+    for opt in opts:
+        if opt[0] == '-f' or opt[0] == '--feed':
+            feedlist.append(opt[1])
+        elif opt[0] == '-d' or opt[0] == '--dir':
+            if path.isdir(opt[1]):
+                savedir = opt[1]
+            else:
+                 print("The provided directory does not exist")
+                 return
+        elif opt[0] == '-v' or opt[0] == '--verbose':
+            verbose += 1
+    print(feedlist)
 
-    nextPage = None
-    for link in feedobj['feed']['links']:
-        if link['rel'] == 'next':
-            nextPage = link['href']
-            break
+    if verbose > 1:
+        print("Verbose level: ", verbose-1)
 
-    for episode in feedobj['items']:
+    for feed in feedlist:
+        if verbose > 0:
+            print("Downloading archive for: " + feed)
+        download_archive(feed)
+    return
 
-        for link in episode['links']:
-            if link['type'] == 'audio/mp4':
-                linklist.append(link['href'])
 
-if verbose:
-    print(" {0:d} links.\n2. Downloading content .."
-                  .format(len(linklist)), end="")
+def download_archive(feed):
+    if (verbose > 0): print("1. Gathering link list ..", end="")
 
-for link in linklist:
-    print(".", end="", flush=True)
-    filename = path.join(savedir, path.basename(link))
+    linklist = []
+    while nextPage is not None:
+        print(".", end="", flush=True)
+        feedobj = feedparser.parse(nextPage)
 
-    # Download the file from `url` and save it locally under `file_name`:
-    with request.urlopen(link) as response, open(filename, 'wb') as outfile:
-        shutil.copyfileobj(response, outfile)
+        nextPage = None
+        for link in feedobj['feed']['links']:
+            if link['rel'] == 'next':
+                nextPage = link['href']
+                break
+
+        for episode in feedobj['items']:
+            for link in episode['links']:
+                if link['type'] == 'audio/mp4':
+                    linklist.append(link['href'])
+                elif link['type'] == 'audio/mp3':
+                    linklist.append(link['href'])
+                elif link['type'] == 'audio/ogg':
+                    linklist.append(link['href'])
+
+    if (verbose > 0):
+        print(" {0:d} episodes.\n2. Downloading content .."
+              .format(len(linklist)), end="")
+
+    for link in linklist:
+        print(".", end="", flush=True)
+        filename = path.join(savedir, path.basename(link))
+
+        with urlopen(link) as response, open(filename, 'wb') as outfile:
+            copyfileobj(response, outfile)
+
+
+if __name__ == "__main__":
+    main()
+
