5a3c017d407039658a066aa6add62e79df347ec1
Jan Willhaus <mail@janwillhaus.de>
2017-01-18 20:17:31 +0100

    Implement url parsing properly (fixes #1)

--- podcast_archiver.py
+++ podcast_archiver.py
@@ -32,6 +32,7 @@ import urllib.error
 from shutil import copyfileobj
 from os import path, remove, makedirs, access, W_OK
 from string import ascii_letters, digits
+from urllib.parse import urlparse
 import re
 
 
@@ -231,17 +232,15 @@ def download_archive(nextPage):
             print("\nDownloading file no. {0}/{1}:\n{2}"
                   .format(cnt + 1, nlinks, link), end="", flush=True)
 
-        # Remove HTTP GET parameters from filename
-        fileparts = path.splitext(path.basename(link))
-        match_gets = re.search('^(.+)(\?.*)$', fileparts[1])
-        if match_gets:
-            fileparts = (fileparts[0], match_gets.group(1))
+        # Remove HTTP GET parameters from filename by parsing URL properly
+        linkpath = urlparse(link).path
+        basename = path.basename(linkpath)
 
         # Generate local path and check for existence
         if subdirs:
-            filename = path.join(curbasedir, fileparts[0] + fileparts[1])
+            filename = path.join(curbasedir, basename)
         else:
-            filename = path.join(savedir, fileparts[0] + fileparts[1])
+            filename = path.join(savedir, basename)
 
         if path.isfile(filename):
             continue
