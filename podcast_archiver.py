22a4ef6897526455071d2c4827124f412215d8f7
Jan Willhaus <mail@janwillhaus.de>
2017-02-13 00:07:19 +0100

    Update copyright interval

--- podcast_archiver.py
+++ podcast_archiver.py
@@ -2,7 +2,7 @@
 """
 Podcast Archiver v0.1: Feed parser for local podcast archive creation
 
-Copyright (c) 2014 Jan Willhaus
+Copyright (c) 2014-2017 Jan Willhaus
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
