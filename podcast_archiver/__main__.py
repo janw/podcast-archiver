7b0f393736358eb9e59c1b7d31e7268f09ca333c
Jan Willhaus <mail@janwillhaus.de>
2023-04-26 19:13:41 +0200

    chore: Factor out argparser, main

--- podcast_archiver/__main__.py
+++ podcast_archiver/__main__.py
@@ -1,3 +1,26 @@
-from podcast_archiver.base import main
+import argparse
+import sys
 
-main()
+from podcast_archiver.argparse import parser
+from podcast_archiver.base import PodcastArchiver
+
+
+def main():
+    try:
+        args = parser.parse_args()
+        if not (args.opml or args.feed):
+            parser.error("Must provide at least one of --feed or --opml")
+
+        pa = PodcastArchiver()
+        pa.addArguments(args)
+        pa.processFeeds()
+    except KeyboardInterrupt:
+        sys.exit("\nERROR: Interrupted by user")
+    except FileNotFoundError as error:
+        sys.exit("\nERROR: %s" % error)
+    except argparse.ArgumentTypeError as error:
+        parser.error(str(error))
+
+
+if __name__ == "__main__":
+    main()
