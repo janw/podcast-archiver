befbac663c15efef8eff412e38cb579abdd53e9d
Jan Willhaus <mail@janwillhaus.de>
2024-12-03 21:31:22 +0100

    fix: Don't add episode to DB if they failed downloading (#209)

--- tests/test_processor.py
+++ tests/test_processor.py
@@ -2,18 +2,22 @@ from __future__ import annotations
 
 from pathlib import Path
 from typing import TYPE_CHECKING
+from unittest import mock
 from unittest.mock import patch
 
 import pytest
 
-from podcast_archiver.config import Settings
 from podcast_archiver.enums import DownloadResult
 from podcast_archiver.models import FeedPage
 from podcast_archiver.processor import FeedProcessor, ProcessingResult
+from podcast_archiver.types import EpisodeResult
 
 if TYPE_CHECKING:
     from pydantic_core import Url
     from responses import RequestsMock
+
+    from podcast_archiver.models import Episode
+    from podcast_archiver.types import EpisodeResultsList
 
 
 @pytest.mark.parametrize(
@@ -31,11 +35,10 @@ def test_preflight_check(
     database_exists: bool,
     expected_result: DownloadResult | None,
 ) -> None:
-    settings = Settings()
     feed = FeedPage.model_validate(feedobj_lautsprecher)
     episode = feed.episodes[0]
     target = Path("file.mp3")
-    proc = FeedProcessor(settings)
+    proc = FeedProcessor()
     if file_exists:
         target.touch()
     with patch.object(proc.database, "exists", return_value=database_exists):
@@ -45,10 +48,59 @@ def test_preflight_check(
 
 
 def test_retrieve_failure(responses: RequestsMock) -> None:
-    settings = Settings()
-    proc = FeedProcessor(settings)
+    proc = FeedProcessor()
 
     result = proc.process("https://broken.url.invalid")
 
     assert result == ProcessingResult()
     assert result.feed is None
+
+
+def test_download_success(tmp_path_cd: Path, feed_lautsprecher: str) -> None:
+    proc = FeedProcessor()
+
+    result = proc.process(feed_lautsprecher)
+
+    assert result != ProcessingResult()
+    assert result.success == 5
+    assert result.feed
+    assert result.feed.url == feed_lautsprecher
+
+
+def test_handle_results_mixed(episode: Episode) -> None:
+    proc = FeedProcessor()
+    episodes: EpisodeResultsList = [
+        EpisodeResult(episode=episode, result=DownloadResult.COMPLETED_SUCCESSFULLY),
+        EpisodeResult(episode=episode, result=DownloadResult.FAILED),
+    ]
+
+    with mock.patch.object(proc.database, "add", return_value=None) as mock_add:
+        success, failures = proc._handle_results(episodes)
+
+    assert success == 1
+    assert failures == 1
+    assert mock_add.call_count == 1
+
+
+def test_handle_results_failure(episode: Episode) -> None:
+    proc = FeedProcessor()
+    episodes: EpisodeResultsList = [EpisodeResult(episode=episode, result=DownloadResult.ABORTED)]
+
+    with mock.patch.object(proc.database, "add", return_value=None) as mock_add:
+        success, failures = proc._handle_results(episodes)
+
+    assert success == 0
+    assert failures == 1
+    mock_add.assert_not_called()
+
+
+def test_handle_results_failed_future(episode: Episode) -> None:
+    proc = FeedProcessor()
+    episodes: EpisodeResultsList = [EpisodeResult(episode=episode, result=DownloadResult.ABORTED)]
+
+    with mock.patch.object(proc.database, "add", return_value=None) as mock_add:
+        success, failures = proc._handle_results(episodes)
+
+    assert success == 0
+    assert failures == 1
+    mock_add.assert_not_called()
